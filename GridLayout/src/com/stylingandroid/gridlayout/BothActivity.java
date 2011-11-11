18145c25c3eef6b6c994e278cd37c9d43f366e2d
Mark Allison <mark@stylingandroid.com>
2011-11-11 15:41:12 +0000

    part 2

---
+++ GridLayout/src/com/stylingandroid/gridlayout/BothActivity.java
@@ -0,0 +1,15 @@
+package com.stylingandroid.gridlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BothActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.both);
+    }
+
+}
